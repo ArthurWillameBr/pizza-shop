4f5d286227220d9458fbbae95c36d8ac46a6b53f
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-23 12:13:58 -0300

    feat: add singIn page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Pizza } from "lucide-react";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth</h1>
-
-      <div>
+    <div className="grid-cols-2 grid min-h-screen">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="size-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col justify-center items-center">
         <Outlet />
       </div>
     </div>

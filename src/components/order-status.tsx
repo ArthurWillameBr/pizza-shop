bdce515b5b53ef46dc1bf02c11a7ef4cb6a79b27
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-24 16:07:59 -0300

    feat: Add OrderStatus component for displaying order status

---
+++ src/components/order-status.tsx
@@ -0,0 +1,34 @@
+type OrderStatus = "pending" | "canceled" | "delivering" | "delivered";
+
+interface OrderStatusProps {
+    status: OrderStatus
+}
+
+const orderStatusMap: Record<OrderStatus, string> = {
+    pending: "Pendente",
+    canceled: "Cancelado",
+    delivering: "Em entrega",
+    delivered: "Entregue",
+};
+
+export function OrderStatus({status}: OrderStatusProps) {
+    return(
+        <div className="flex items-center gap-2">
+        {status === "pending" && (
+            <span className="size-2 rounded-full bg-slate-400" />
+        )}
+        {status === "canceled" && (
+            <span className="size-2 rounded-full bg-rose-500" />
+        )}
+        {status === "delivered" && (
+            <span className="size-2 rounded-full bg-green-500" />
+        )}
+        {["processing", "delivering"].includes(status) && (
+            <span className="size-2 rounded-full bg-amber-500" />
+        )}
+
+        <span className="text-medium text-muted-foreground ">{orderStatusMap[status]}</span>
+      </div>
+    )
+
+}

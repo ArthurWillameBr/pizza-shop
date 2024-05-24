e888502f497f9bae0ecb661bd0ee38ca662755e4
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-24 17:28:01 -0300

    feat: Update Orders component with lazy loading for orders and pagination

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -11,46 +11,80 @@ import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
 import { useQuery } from "@tanstack/react-query";
 import { GetOrders } from "@/api/get-orders";
+import { useSearchParams } from "react-router-dom";
+import { z } from "zod";
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const { data: results } = useQuery({
-    queryKey: ["orders"],
-    queryFn: GetOrders,
-  })
+  const orderId = searchParams.get("orderId");
+  const customerName = searchParams.get("customerName");
+  const status = searchParams.get("status");
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get("page") ?? "1");
+
+  const { data: result } = useQuery({
+    queryKey: ["orders", pageIndex, orderId, customerName, status],
+    queryFn: () =>
+      GetOrders({
+        pageIndex,
+        customerName,
+        orderId,
+        status: status === "all" ? null : status,
+      }),
+  });
+
+  function handlePageChange(pageIndex: number) {
+    setSearchParams((prev) => {
+      prev.set("page", (pageIndex + 1).toString());
+
+      return prev;
+    });
+  }
 
   return (
     <>
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      
-      <div className="space-y-2.5">
-        <OrderTableFilters />
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">Identificador</TableHead>
-                <TableHead className="w-[180px]">Realizado há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {results && results.orders.map((order) => {
-                return <OrderTableRow key={order.orderId}  order={order} />;
-              })}
-            </TableBody>
-          </Table>
-        </div>
 
-        <Pagination pageIndex={0} totalCount={105} perPage={10}/>
-      </div>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />;
+                  })}
+              </TableBody>
+            </Table>
+          </div>
+
+          {result && (
+            <Pagination
+              onPageChange={handlePageChange}
+              pageIndex={result.meta.pageIndex}
+              perPage={result.meta.perPage}
+              totalCount={result.meta.totalCount}
+            />
+          )}
+        </div>
       </div>
     </>
   );

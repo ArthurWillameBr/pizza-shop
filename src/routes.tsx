c16e331797f5a2afdd6c6037a413d7b53f35180d
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-24 16:07:07 -0300

    feat: Update routes.tsx with corrected import statements and component names

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard/dashboard";
-import { SingIn } from "./pages/auth/singIn";
+import { SignIn } from "./pages/auth/signIn";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
-import { SingUp } from "./pages/auth/singUp";
+import { SignUp } from "./pages/auth/signUp";
 import { Orders } from "./pages/app/orders/orders";
 import { NotFound } from "./pages/404";
 
@@ -22,8 +22,8 @@ export const router = createBrowserRouter([
     path: "/",
     element: <AuthLayout />,
     children: [
-      { path: "sing-in", element: <SingIn /> },
-      { path: "sing-up", element: <SingUp /> }
+      { path: "sign-in", element: <SignIn /> },
+      { path: "sign-up", element: <SignUp /> }
   ],
   },
 ]);

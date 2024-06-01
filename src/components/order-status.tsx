abec93b5c138d5f1ab1cb82f96ef0f0818a1a1d1
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-01 10:34:02 -0300

    feat: Refactor OrderStatus component to improve code readability and maintainability

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -1,35 +1,41 @@
-type OrderStatus = "pending" | "canceled" | "delivering" | "delivered" | "processing";
+export type OrderStatus =
+  | "pending"
+  | "canceled"
+  | "delivering"
+  | "delivered"
+  | "processing";
 
 interface OrderStatusProps {
-    status: OrderStatus
+  status: OrderStatus;
 }
 
 const orderStatusMap: Record<OrderStatus, string> = {
-    pending: "Pendente",
-    canceled: "Cancelado",
-    delivering: "Em entrega",
-    delivered: "Entregue",
-    processing: "Em preparo"
+  pending: "Pendente",
+  canceled: "Cancelado",
+  delivering: "Em entrega",
+  delivered: "Entregue",
+  processing: "Em preparo",
 };
 
-export function OrderStatus({status}: OrderStatusProps) {
-    return(
-        <div className="flex items-center gap-2">
-        {status === "pending" && (
-            <span className="size-2 rounded-full bg-slate-400" />
-        )}
-        {status === "canceled" && (
-            <span className="size-2 rounded-full bg-rose-500" />
-        )}
-        {status === "delivered" && (
-            <span className="size-2 rounded-full bg-green-500" />
-        )}
-        {["processing", "delivering"].includes(status) && (
-            <span className="size-2 rounded-full bg-amber-500" />
-        )}
+export function OrderStatus({ status }: OrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      {status === "pending" && (
+        <span className="size-2 rounded-full bg-slate-400" />
+      )}
+      {status === "canceled" && (
+        <span className="size-2 rounded-full bg-rose-500" />
+      )}
+      {status === "delivered" && (
+        <span className="size-2 rounded-full bg-green-500" />
+      )}
+      {["processing", "delivering"].includes(status) && (
+        <span className="size-2 rounded-full bg-amber-500" />
+      )}
 
-        <span className="text-medium text-muted-foreground ">{orderStatusMap[status]}</span>
-      </div>
-    )
-
+      <span className="text-medium text-muted-foreground ">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  );
 }

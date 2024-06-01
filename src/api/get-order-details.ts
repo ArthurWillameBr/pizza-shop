645a90ed7e62e4bbf8e658ce9eea6b0ca8c0c547
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-01 10:37:39 -0300

    feat: Add getOrderDetails API function for retrieving order details

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,30 @@
+import { api } from "@/lib/axios";
+
+export interface getOrderDetailsParams {
+    orderId: string;
+}
+export interface getOrderDetailsResponse {
+    id: string,
+    createdAt: string,
+    status: "pending" | "canceled" | "delivering" | "delivered" | "processing",
+    totalInCents: number,
+    customer: {
+        name: string,
+        email: string,
+        phone: string | null
+    },
+    orderItems: {
+        id: string,
+        priceInCents: number,
+        quantity: number,
+        product: {
+            name: string,
+        }
+    }[]
+}
+
+export async function getOrderDetails({orderId}: getOrderDetailsParams) {
+   const response = await api.get<getOrderDetailsResponse>(`/orders/${orderId}`);
+
+    return response.data;
+}

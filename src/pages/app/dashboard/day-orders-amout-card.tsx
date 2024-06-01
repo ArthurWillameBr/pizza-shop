e403f5d8c19e26e1edf218ab09651cacef2edb4d
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-01 10:39:33 -0300

    feat: Update DayOrderAmountCard component to fetch and display daily order amounts

--- src/pages/app/dashboard/day-orders-amout-card.tsx
+++ src/pages/app/dashboard/day-orders-amout-card.tsx
@@ -1,21 +1,46 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { Utensils } from "lucide-react";
+import { getDayOrdersAmount } from "@/api/get-day-orders-amount";
+import { MetricCardSkeleton } from "./metric-card-skeleton";
 
 export function DayOrderAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryKey: ['metrics', 'day-orders-amount'],
+    queryFn: getDayOrdersAmount,
+  });
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-base font-semibold">
-          Pedidos (dia)
-        </CardTitle>
+        <CardTitle className="text-base font-semibold">Pedidos (dia)</CardTitle>
         <Utensils className="size-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">47</span>
-        <p className="text-sm text-muted-foreground">
-          <span className="text-rose-500 dark:text-rose-400">-6%</span> em
-          relação a ontem
-        </p>
+        {dayOrdersAmount ? (
+          <>
+            <span className="text-2xl font-bold">
+              {dayOrdersAmount.amount.toLocaleString("pt-BR")}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              <span
+                className={
+                  dayOrdersAmount.diffFromYesterday > 0
+                    ? "text-emerald-500"
+                    : "text-red-500"
+                }
+              >
+                {dayOrdersAmount.diffFromYesterday > 0
+                  ? `+${dayOrdersAmount.diffFromYesterday}`
+                  : dayOrdersAmount.diffFromYesterday}
+                %
+              </span>{" "}
+              em relação a ontem
+            </p>
+          </>
+        ) : (
+          <MetricCardSkeleton/>
+        )}
       </CardContent>
     </Card>
   );

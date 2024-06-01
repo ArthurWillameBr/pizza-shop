c3192866710152d7c5a5da707d87c630af7aea49
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-01 10:40:08 -0300

    feat: Update MonthOrderAmountCard to fetch and display monthly order amounts

--- src/pages/app/dashboard/month-orders-amout-card.tsx
+++ src/pages/app/dashboard/month-orders-amout-card.tsx
@@ -1,7 +1,15 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { Utensils } from "lucide-react";
+import { getMonthOrdersAmount } from '@/api/get-mouth-orders-amount'
+import { MetricCardSkeleton } from "./metric-card-skeleton";
+
 
 export function MonthOrderAmountCard() {
+  const { data: monthOrdersAmount } = useQuery({
+    queryFn: getMonthOrdersAmount,
+    queryKey: ['metrics', 'month-orders-amount'],
+  });
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,11 +19,27 @@ export function MonthOrderAmountCard() {
         <Utensils className="size-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">247</span>
-        <p className="text-sm text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">+6%</span> em
-          relação ao mês anterior
-        </p>
+      {monthOrdersAmount ? (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthOrdersAmount.amount.toLocaleString("pt-BR")}
+            </span>
+            <p className="text-sm text-muted-foreground">
+              {monthOrdersAmount.diffFromLastMonth > 0 ? (
+                <span className="text-green-500 dark:text-green-400">
+                  +{monthOrdersAmount.diffFromLastMonth}%
+                </span>
+              ) : (
+                <span className="text-rose-500 dark:text-red-400">
+                  -{monthOrdersAmount.diffFromLastMonth}%
+                </span>
+              )}
+              {" "}em relação ao mês passado
+            </p>
+          </>
+        ) : (
+          <MetricCardSkeleton/>
+        )}
       </CardContent>
     </Card>
   );

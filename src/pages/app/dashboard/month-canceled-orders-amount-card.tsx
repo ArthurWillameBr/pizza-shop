3e6182cc3f2b455349dec9af201e55a2d09d25d0
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-01 10:40:01 -0300

    feat: Update MonthCanceledOrdersAmountCard to fetch and display monthly canceled orders amount

--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,7 +1,15 @@
+import { getMonthCanceledOrdersAmount } from "@/api/get-month-canceled-orders-amount";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { DollarSignIcon } from "lucide-react";
+import { MetricCardSkeleton } from "./metric-card-skeleton";
 
 export function MonthCanceledOrdersAmountCard() {
+
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryFn: getMonthCanceledOrdersAmount,
+    queryKey: ['metrics', 'month-canceled-orders-amount'],
+  });
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,11 +19,27 @@ export function MonthCanceledOrdersAmountCard() {
         <DollarSignIcon className="size-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">32</span>
-        <p className="text-sm text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">-6%</span> em
-          relação ao mês anterior
-        </p>
+      {monthCanceledOrdersAmount ? (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthCanceledOrdersAmount.amount.toLocaleString("pt-BR")}
+            </span>
+            <p className="text-sm text-muted-foreground">
+              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
+                <span className="text-green-500 dark:text-green-400">
+                  {monthCanceledOrdersAmount.diffFromLastMonth}%
+                </span>
+              ) : (
+                <span className="text-rose-500 dark:text-red-400">
+                  +{monthCanceledOrdersAmount.diffFromLastMonth}%
+                </span>
+              )}
+              {" "}em relação ao mês passado
+            </p>
+          </>
+        ) : (
+          <MetricCardSkeleton/>
+        )}
       </CardContent>
     </Card>
   );
